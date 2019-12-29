a388787cf6e593d3d3dd52701f525eeaa225b369
ilayFriedman <ilayfri@bgu.ac.il>
2019-12-29 16:54:07 +0200

    almost end of GUI

--- frontend.py
+++ frontend.py
@@ -13,22 +13,43 @@ from kivy.uix.popup import Popup
 
 class MyGrid(FloatLayout):
     def openPopUp(self):
-        show_popup()
-    pass
+        show_res_popup()
 
 class Popups(FloatLayout):
     pass
-def show_popup():
-    print("user: ", ids.user_input.text)
-    print("password: ", self.ids.password_input.text)
+
+class ErrorPage(FloatLayout):
+    pass
+
+def show_res_popup():
     show = Popups()
     popupWindow = Popup(title="Popup Window", content=show,
                         size_hint=(None, None), size=(200, 200))
     popupWindow.open()
+
 class MyApp(App):
     def build(self):
-        return MyGrid()
+        self.UI = MyGrid()
+        return self.UI
 
+    def openPopUpSubmit(self):
+        if(self.root.ids.count_res_input.text.isdigit() and len(self.root.ids.time_input.text) > 0 and len(self.root.ids.location_input.text) > 0):
+            self.loc = self.root.ids.location_input.text
+            self.duration = self.root.ids.time_input.text
+            self.resCount = self.root.ids.count_res_input.text
+
+            self.root.ids.location_input.text = ''
+            self.root.ids.time_input.text = ''
+            self.root.ids.count_res_input.text = ''
+            self.UI.openPopUp()
+        else:
+            show = ErrorPage()
+            self.popupWindowERR = Popup(title="OOPS!", content=show,
+                                size_hint=(None, None), size=(400, 260))
+            self.popupWindowERR.open()
+
+    def closePopUp(self):
+        self.popupWindowERR.dismiss()
 
 if __name__ == "__main__":
     MyApp().run()
